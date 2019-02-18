0a0c1bb04367e5caa0133645ce32b346c56a8bbe
Stu Field <stu.g.field@gmail.com>
2019-02-18 07:00:09 -0700

    :package: added and fixed a :bug:

--- tests/testthat/test-camelCaseFunction.R
+++ tests/testthat/test-camelCaseFunction.R
@@ -5,7 +5,11 @@ test_that("test failure control", {
   expect_true(TRUE)
 })
 
+print(Sys.getlocale("LC_COLLATE"))
+Sys.setlocale(locale = "en_US.UTF-8")
+print(Sys.getlocale("LC_COLLATE"))
+
 test_that("sort() ignores case", {
-  x <- c("ABC", "ZAP", "null")
-  expect_equal(sort(x), c("ABC", "null", "ZAP"))
+  x <- c("Aaron", "Zack", "nancy")
+  expect_equal(sort(x), c("Aaron", "nancy", "Zack"))
 })
