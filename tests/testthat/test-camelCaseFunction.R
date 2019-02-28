8fe90be99e366153bd399637083f8402bb74a943
Stu Field <stu.g.field@gmail.com>
2019-02-28 12:04:40 -0700

    Fix README.Rmd for pkgdown

--- tests/testthat/test-camelCaseFunction.R
+++ tests/testthat/test-camelCaseFunction.R
@@ -5,9 +5,9 @@ test_that("test failure control", {
   expect_true(TRUE)
 })
 
-print(Sys.getlocale("LC_COLLATE"))
-Sys.setlocale(locale = "en_US.UTF-8")
-print(Sys.getlocale("LC_COLLATE"))
+#print(Sys.getlocale("LC_COLLATE"))
+#Sys.setlocale(locale = "en_US.UTF-8")
+#print(Sys.getlocale("LC_COLLATE"))
 
 test_that("sort() ignores case", {
   x <- c("Aaron", "Zack", "nancy")
