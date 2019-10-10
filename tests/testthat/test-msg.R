00f2bdd00775692c0c67cb363166fbeef3ee2880
Stu Field <stu.g.field@gmail.com>
2019-10-10 12:45:23 -0600

    Clean up case-sensitive sorting unit test
    - rename to test-msg.R
    - use 'withr::with_locale()'

--- tests/testthat/test-msg.R
+++ tests/testthat/test-msg.R
@@ -1,11 +1,21 @@
-context("test-msg")
+context("test locale and case-sensitive sorting")
 
-test_that("test failure control", {
-  # expect_true(FALSE)
-  expect_true(TRUE)
+x <- c("Aaron", "Zack", "nancy", "clark")
+
+test_that("sort() ignores case via locale = 'en_US.UTF-8'", {
+  withr::with_locale(
+    c(LC_COLLATE = "en_US.UTF-8"), {
+      expect_equal(Sys.getlocale("LC_COLLATE"), "en_US.UTF-8") # sanity check
+      expect_equal(sort(x), c("Aaron", "clark", "nancy", "Zack"))
+    }
+  )
 })
 
-test_that("sort() ignores case", {
-  x <- c("Aaron", "Zack", "nancy")
-  expect_equal(sort(x), c("Aaron", "nancy", "Zack"))
+test_that("sort() does not ignore case via locale = 'C'", {
+  withr::with_locale(
+    c(LC_COLLATE = "C"), {
+      expect_equal(Sys.getlocale("LC_COLLATE"), "C")    # sanity check
+      expect_equal(sort(x), c("Aaron", "Zack", "clark", "nancy"))
+    }
+  )
 })
