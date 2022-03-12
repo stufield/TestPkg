4b1470f54b75955e3f553b78aa64d4c4d6148b81
Stu Field <stu.g.field@gmail.com>
2022-03-12 08:32:26 -0700

    Add new `create_release_issue() utility
    - creates an issue checklist for releases
    - based on `usethis::use_release_issue()`

--- R/TestPkg-package.R
+++ R/TestPkg-package.R
@@ -5,3 +5,39 @@
 #' @useDynLib TestPkg, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 NULL
+
+# for use with usethis::use_release_issue
+release_bullets <- function() {
+  c(
+    "Submit PR to `master` branch",
+    "Merge PR via 'Rebase and merge'",
+    "Add next release version tag",
+    "Add release to public repo :partying_face:",
+    ":tada:"
+  )
+}
+
+checklist <- function() {
+  c("Prepare for release:",
+    "",
+    paste("- [ ]", release_bullets())
+  )
+}
+
+create_release_issue <- function(ver = NULL) {
+  stopifnot(!is.null(ver))
+  projfile <- dir(pattern = "[.]Rproj$")
+  project  <- gsub("\\.Rproj$", "", projfile)
+  issue <- gh::gh(
+    endpoint = "POST /repos/{owner}/{repo}/issues",
+    owner    = "stufield",
+    repo     = "TestPkg",
+    .api_url = "https://api.github.com",
+    title    = sprintf("Release %s %s", project, ver),
+    body     = paste0(checklist(), "\n", collapse = "")
+  )
+  if ( interactive() ) {
+    Sys.sleep(1)
+    utils::browseURL(issue$html_url)
+  }
+}
